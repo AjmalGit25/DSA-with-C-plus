fd54b5b2f6d7050a2d86ffc989abb611639fd656
Md Ajmal Hussain <ajmalashrafi2016@gmail.com>
2026-05-25 00:02:00 +0530

    'Erase Overlap Intervals (return int)' code add

---
+++ Array/Erase Overlap Intervals (return int).cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int eraseOverlapIntervals (vector<vector<int>>& intervals) {
+    if (intervals.empty()) 
+        return 0;
+    
+    // 1. Sort by END
+    sort (intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b){
+        return a[1] < b[1];
+    });
+    
+    int count = 0;
+    int prev_end = intervals[0][1];
+    
+    for (int i = 1; i < intervals.size(); i++) {
+        // overlap -> increase count
+        if (intervals[i][0] < prev_end)
+            count++;
+        else 
+            prev_end = intervals[i][1];
+    }
+    
+    return count;
+}
+
+int main () {
+    vector<vector<int>> intervals = {
+        {1, 2},
+		{2, 3},
+		{3, 4},
+		{1, 3}
+    };
+    
+    vector<int> newInterval = {4,8};
+    
+    int res = eraseOverlapIntervals (intervals);
+    
+    cout << res;
+
+    return 0;
+}
