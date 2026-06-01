63b4c80a17e743765bd9566d4c4c1342b8cddab8
Md Ajmal Hussain <ajmalashrafi2016@gmail.com>
2026-06-01 06:00:22 +0530

    Learned how to move zeroes with best approach

---
+++ Array/Move All Zero to End.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Naive Approach - using Temporary Array - O(n) Time and O(n) Space
+vector<int> moveZeroes (vector<int>& nums) {
+    int n = nums.size();
+    
+    vector<int> temp(n);        // n elements, all initialized to 0
+    
+    // to keep track of the index in temp[]
+    int j = 0;
+    
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != 0) {
+            temp[j++] = nums[i];
+        }
+    }
+    
+    return temp;
+}
+
+// Better Approach - Two Traversals-O(n) Time and O(1) space
+void moveZeroes (vector<int>& nums) {
+    int n = nums.size();
+    
+   
+    int count = 0;
+    
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != 0) {
+            nums[count] = nums[i];
+            count++;
+        }
+    }
+    
+    // fill remaining elements with 0
+    while (count < n) {
+        nums[count] = 0;
+        count++;
+    }
+}
+
+int main () {
+    vector<int> nums = {1, 2, 0, 4, 3, 0, 5, 0};
+    
+    vector<int> result = moveZeroes (nums);
+    
+    cout << "Result: \n";
+    
+    for (const auto& val : result) {
+        cout << val << " ";
+    }
+    
+    return 0;
+}
