b05126d8c4b893da3a668b35d700ac0ce0546e55
Md Ajmal Hussain <ajmalashrafi2016@gmail.com>
2026-06-08 00:59:56 +0530

    bug fixed

--- Array/Next Permutation.cpp
+++ Array/Next Permutation.cpp
@@ -1,22 +1,23 @@
 void nextPermutation (vector<int>& nums) {
     int n = nums.size();
     int i = n - 2;
-
+    
     // Step 1: find pivot
-    while (i >= 0 && nums[i] >= nums[i + 1]) {
+    while (i >= 0 && nums[i] >= nums[i + 1])
         i--;
-    }
-
-    // Step 2: find next greater
+    
     if (i >= 0) {
         int j = n - 1;
-        while (nums[j] <= nums[i]) {
+        
+        // Step 2: find next greater
+        while (nums[j] <= nums[i])
             j--;
-        }
+            
+        // Step 3: swap
         swap (nums[i], nums[j]);
     }
-
-    // Step 3: reverse right part
+    
+    // Step 4: reverse right part
     reverse (nums.begin() + i + 1, nums.end());
 }
 
