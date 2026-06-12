555976ff94dc8e2598bed80aec14b1fe4e23e54e
Md Ajmal Hussain <ajmalashrafi2016@gmail.com>
2026-06-12 12:47:33 +0530

    new better approach with linear O(n) complexity learned

--- Array/Buy or Sell Stock.cpp
+++ Array/Buy or Sell Stock.cpp
@@ -1,23 +1,46 @@
 #include <iostream>
+#include <vector>
 #include <climits>
 using namespace std;
 
+// [Naive Approach] By exploring all possible pairs - O(n^2) Time and O(1) Space
 int maxProfit (vector<int>& prices) {
-	int n = prices.size();
-	int profit = 0;
+    int n = prices.size();
+    
+    int profit = 0;
+    
+    // Explore all possible ways to buy and sell stock
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            profit = max (profit, prices[j] - prices[i]);
+        }
+    }
+    
+    return profit;
+}
 
-	// Explore all possible ways to buy and sell stock
-	for (int i = 0; i < n - 1; i++) {
-		for (int j = i + 1; j < n; j++) {
-			res = max (profit, prices[j] - prices[i]);
-		}
-	}
-	
-	return profit;
+
+// [Expected Approach] One Traversal Solution - O(n) Time and O(1) Space
+int maxProfit (vector<int>& prices) {
+    int n = prices.size();
+    
+    int minSoFar = prices[0];
+    int profit = 0;
+    
+    for (int i = 1; i < n; i++) {
+        
+        // Update the minimum value seen so far
+        minSoFar = min (minSoFar, prices[i]);
+        
+        // Update result if we get more profit
+        profit = max (profit, prices[i] - minSoFar);
+    }
+    
+    return profit;
 }
 
 int main () {
-    vector<int> prices = {7,1,5,3,6,4};
+    vector<int> prices = {7, 10, 1, 3, 6, 9, 2};
     
     int result = maxProfit (prices);
     
